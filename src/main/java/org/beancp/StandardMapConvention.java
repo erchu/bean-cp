aaf9f0226dce7b027f48a0604bd0a29652b2775e
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-04-19 00:14:35 +0200

    Javadoc fixes

--- src/main/java/org/beancp/StandardMapConvention.java
+++ src/main/java/org/beancp/StandardMapConvention.java
@@ -41,42 +41,74 @@ public class StandardMapConvention implements MapConvention {
     }
 
     /**
-     * Returns default mapping convention:
+     * Returns mapping convention with following configuration:
+     * 
+     * <p>
      * <ul>
-     * <li>No destination members excludes</li>
+     * <li>No destination members excluded</li>
      * <li>Maximum possible number of destination members included</li>
      * <li>Will <b>not</b> fail if not all <b>destination</b> members are mapped</li>
      * <li>Will <b>not</b> fail if not all <b>source</b> members are mapped</li>
-     * <li>Flatening feature disabled</li>
+     * <li>Flattening feature <b>enabled</b></li>
      * <li>Will cast or map if possible for members of different data type (see
      * {@link #castOrMapIfPossible()} method)</li>
      * </ul>
+     * </p>
      *
-     * @return default mapping convention.
+     * @return mapping convention.
      */
-    public static StandardMapConvention getDefault() {
+    public static StandardMapConvention getMaximumFlexibilityMatch() {
         StandardMapConvention defaultConvention = new StandardMapConvention();
         defaultConvention.excludeDestinationMembers = new String[0];
         defaultConvention.includeDestinationMembers = new String[0];
         defaultConvention.failIfNotAllDestinationMembersMapped = false;
         defaultConvention.failIfNotAllSourceMembersMapped = false;
-        defaultConvention.flateningEnabled = false;
+        defaultConvention.flateningEnabled = true;
         defaultConvention.castOrMapIfPossible = true;
 
         return defaultConvention;
     }
 
     /**
-     * Sets list of destination members which will be included by convention. Each member must be
+     * Returns mapping convention with following configuration
+     * 
+     * <p>
+     * <ul>
+     * <li>No destination members excluded</li>
+     * <li>Maximum possible number of destination members included</li>
+     * <li>Will <b>not</b> fail if not all <b>destination</b> members are mapped</li>
+     * <li>Will <b>not</b> fail if not all <b>source</b> members are mapped</li>
+     * <li>Flattening feature <b>disabled</b></li>
+     * <li>Will <b>not</b> cast or map if possible for members of different data type (see
+     * {@link #castOrMapIfPossible()} method)</li>
+     * </ul>
+     * </p>
+     *
+     * @return mapping convention.
+     */
+    public static StandardMapConvention getStrictMatch() {
+        StandardMapConvention defaultConvention = new StandardMapConvention();
+        defaultConvention.excludeDestinationMembers = new String[0];
+        defaultConvention.includeDestinationMembers = new String[0];
+        defaultConvention.failIfNotAllDestinationMembersMapped = false;
+        defaultConvention.failIfNotAllSourceMembersMapped = false;
+        defaultConvention.flateningEnabled = false;
+        defaultConvention.castOrMapIfPossible = false;
+
+        return defaultConvention;
+    }
+
+    /**
+     * Sets list of destination members which will be included to matching. Each entry must be
      * regular expression matching field name or bean property name (according to beans
-     * specification). If <b>not specified</b> (empty array) all subject to map by convention. If
-     * <b>specified</b> (not empty array) only members with names matching any of {@code members}
-     * could be mapped by convention. This list has lower priority that exclude list specified by
-     * {@link #excludeDestinationMembers(java.lang.String...)} method.
+     * specification). If <b>not specified</b> (empty array) all members are subject to map by
+     * convention. If <b>specified</b> (not empty array) only members with names matching any of
+     * {@code members} could be mapped by convention. This list has lower priority that exclude list
+     * specified by {@link #excludeDestinationMembers(java.lang.String...)} method.
      *
      * <p>
-     * Note that when you put some member on list then it is not guaranteed that it will be mapped -
-     * it still have to have matching source's member according to convention configuration.
+     * Note that when you put some member on list then it is not guaranteed that it will be mapped
+     * &#8212; it still have to have matching source's member according to convention configuration.
      * </p>
      *
      * @param members members to include
@@ -94,14 +126,14 @@ public class StandardMapConvention implements MapConvention {
     }
 
     /**
-     * Sets list of destination members which will be excluded (ignored) by convention. Each member
+     * Sets list of destination members which will be excluded (ignored) by convention. Each entry
      * must be regular expression matching field name or bean property name (according to beans
      * specification). This list has higher priority that include list specified by {@link #includeDestinationMembers(java.lang.String...)
      * } method.
      *
      * <p>
-     * Note that when you put some member on list then it is not guaranteed that it will be mapped -
-     * it still have to have matching source's member according to convention configuration.
+     * Note that when you put some member on list then it is not guaranteed that it will be mapped
+     * &#8212; it still have to have matching source's member according to convention configuration.
      * </p>
      *
      * @param members members to ignore
@@ -119,9 +151,9 @@ public class StandardMapConvention implements MapConvention {
     }
 
     /**
-     * Enables flattening feature. This feature will try to match members from inner classes only if
-     * no direct member can be matched. This is useful if you have complex model to be mapped to
-     * simpler one. Destination member will be matched to source inner class member if destination
+     * Enables flattening feature. This feature will try to match members from nested classes only
+     * if no direct member can be matched. This is useful if you have complex model to be mapped to
+     * simpler one. Destination member will be matched to source nested class member if destination
      * member's name parts match path to source member. For example: for below classes
      * {@code setCustomerName} will be matched to {@code getCustomer().getName()} because
      * {@code CustomerName} property can be interpreted as path to {@code Customer} and then to
@@ -186,7 +218,8 @@ public class StandardMapConvention implements MapConvention {
     }
 
     /**
-     * Will fail during mapping if not all destination properties are mapped.
+     * Convention will fail during map building (see {@link #build(org.beancp.Mapper, java.lang.Class, java.lang.Class) method)
+     * if not all destination properties are mapped.
      *
      * @return this (for method chaining)
      */
@@ -197,7 +230,8 @@ public class StandardMapConvention implements MapConvention {
     }
 
     /**
-     * Will fail during mapping if not all source properties are mapped.
+     * Convention will fail during map building (see {@link #build(org.beancp.Mapper, java.lang.Class, java.lang.Class) method)
+     * if not all source properties are mapped.
      *
      * @return this (for method chaining)
      */
@@ -211,12 +245,15 @@ public class StandardMapConvention implements MapConvention {
      * For matching members names, but of different data types will try cast or map members wherever
      * it is possible. If matching members are of different types mapper will try one of the
      * following techniques to perform mapping:
+     * 
+     * <p>
      * <ul>
-     * <li>Cast operation between primitive types</li>
-     * <li>Converting values to String</li>
-     * <li>Parsing String values for primitive types</li>
-     * <li>Mapping types using available mapper</li>
+     * <li>Cast between primitive types</li>
+     * <li>Convert values to string</li>
+     * <li>Parse string value (only for primitive types)</li>
+     * <li>Map types using available mapper</li>
      * </ul>
+     * </p>
      *
      * <p>
      * This option is opposite to {@link #mapOnlySameTypeMembers()}.
