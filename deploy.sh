b160b444fe67faa65c5ebb6a9e846c0719c14d57
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-05-25 23:02:30 +0200

    Deploy fix

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ cp -R $HOME/build/erchu/bean-cp/target/apidocs/* ./apidocs
 # Commit and push the changes
 
 git add . -all
-git commit -m "Lastest jar on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER: auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
