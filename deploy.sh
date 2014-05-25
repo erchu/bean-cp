5ef38b6be74c859c4dfcdc765a5e9f732ce89304
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-05-25 22:28:53 +0200

    Deploy script - fix

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,14 @@ git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/erchu/bean-cp gh-pages > /dev/null
 
 # Copy and prepare files to push
+
 cd $HOME/gh-pages
 git rm -rf ./apidocs/*
 cp $HOME/build/erchu/bean-cp/target/beancp-*.jar ./upload
 cp -R $HOME/build/erchu/bean-cp/target/apidocs/* ./apidocs
 
 # Commit and push the changes
+
 git add .
 git commit -m "Lastest jar on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
