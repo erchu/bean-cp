e9cbc0e99af062a273046e949134afd0dc3a753b
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-05-25 22:24:24 +0200

    Deploy script - fix

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,20 @@ cd $HOME/build/erchu/bean-cp
 mvn javadoc:javadoc
 mvn javadoc:jar
 
+# Checkout gh-pages
+
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/erchu/bean-cp gh-pages > /dev/null
 
-# Prepare files
+# Prepare files to push
 cd $HOME/gh-pages/upload
 git rm -rf ./javadoc
 cp $HOME/build/erchu/bean-cp/target/beancp-*.jar ./upload
 cp -R $HOME/build/erchu/bean-cp/target/apidocs/* ./apidocs
 
-# Commit and Push the Changes
+# Commit and push the changes
 git add .
 git commit -m "Lastest jar on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
