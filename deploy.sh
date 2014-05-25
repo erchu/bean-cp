2657b8036d3ec9ee42860a1235f14123e46148d5
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-05-25 21:53:25 +0200

    Deploy script added

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/erchu/bean-cp gh-pages > /dev/null
+
+# Commit and Push the Changes
+cd gh-pages/upload
+cp $HOME/target/beancp-0.1.jar ./upload
+git commit -am "Lastest jar on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
