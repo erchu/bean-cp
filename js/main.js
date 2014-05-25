b64f1679a9bb9f36a461a9e489d390a7a9e4d4c7
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-05-25 22:37:42 +0200

    Link updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
-	$("#page-menu  a").click(function(e) {
-		e.preventDefault();
-		alert("Not available for now");
+	$("#page-menu a").click(function(e) {
+		if ($(this).attr("href") === "#") {
+			e.preventDefault();
+			alert("Not available for now");
+		}
 	});
 });
