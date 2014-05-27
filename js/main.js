75898d65f5588c850af4ab2fe95d7f5d4682d53a
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-05-27 20:05:53 +0200

    API specs diaglo added

--- js/main.js
+++ js/main.js
@@ -5,4 +5,31 @@ $( document ).ready(function() {
 			alert("Not available for now");
 		}
 	});
+	
+	function showApiSpecs() {
+		var apiDialog = $("#dialog");
+		var width = $(window).width();
+		var height = $(window).height();
+
+		width = width - 50;
+		height = height - 120;
+
+		$(apiDialog.children("iframe").get(0)).css("height", height + "px");
+
+		apiDialog.dialog({
+			modal: true,
+			height: "auto", // Set the height to auto so that it grows along with the iframe.
+			width: width
+		});
+	}
+	
+	$('#api-specs-link').click(function(e) {
+		e.preventDefault();
+		showApiSpecs();
+	});
+	
+	$('#footer-api-specs-link').click(function(e) {
+		e.preventDefault();
+		showApiSpecs();
+	});
 });
