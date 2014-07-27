c7745d0d8adcd99a989f5490d1a6dd20ba27494b
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-07-27 11:15:23 +0200

    Test server refactoring

--- start-test-server.js
+++ start-test-server.js
@@ -12,37 +12,43 @@ var mimeTypes = {
     "js": "text/javascript",
     "css": "text/css" };
 	
-http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
 
-  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
+  var uri = url.parse(request.url).pathname;
+  var filePath = path.join(process.cwd(), uri);
   
-  fs.exists(filename, function(exists) {
+  fs.exists(filePath, function(exists) {
     if (!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
       response.end();
+	  
       return;
     }
 
-    if (fs.statSync(filename).isDirectory()) {
-		filename += '/index.html';
+    if (fs.statSync(filePath).isDirectory()) {
+		filePath += '/index.html';
 	}
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if (err) {        
+    fs.readFile(filePath, "binary", function(errorInfo, file) {
+      if (errorInfo) {        
         response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
+        response.write(errorInfo + "\n");
         response.end();
+		
         return;
       }
 	  
-	  var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+	  var fileExtension = path.extname(filePath).split(".")[1];
+	  var mimeType = mimeTypes[fileExtension];
 
       response.writeHead(200, {"Content-Type": mimeType});
       response.write(file, "binary");
       response.end();
     });
   });
-}).listen(parseInt(port, 10));
+});
+
+server.listen(parseInt(port, 10));
 
 console.log("Static file server running at http://localhost:" + port + "/\nCTRL + C to shutdown");
