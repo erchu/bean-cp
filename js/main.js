77cf78917fc1840421bb64898f46abcce25eb6ff
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-09-01 22:34:12 +0200

    Scroll to top feature

--- js/main.js
+++ js/main.js
@@ -1,71 +1,94 @@
 $(document).ready(function() {
 	var app = Sammy('#page-main', function() {
+		var $pageMain = $("#page-main");
+		var $pageMenu = $("#page-menu");
+		var $root = $("html,body");
+		var $body = $("body");
+		var $window = $(window);
+		var $document = $(document);
+		var $scrollToTop = $("#scroll-to-top");
+		var scrollToTopVisible = false;
 	
-	var $pageMain = $("#page-main");
-	var $pageMenu = $("#page-menu");
-	var $root = $("html,body");
-	var $body = $("body");
-	var $window = $(window);
-	var $document = $(document);
-	
-	var currentPage = null;
-	
-	function _scrollToAnchor(address) {
-		var anchor = pageList.getAnchorOnPage(address);
+		var currentPage = null;
 		
-		if (anchor != null) {
-			var targetOffset = $("#" + anchor).offset().top - 20;
+		function _scrollToAnchor(address) {
+			var anchor = pageList.getAnchorOnPage(address);
 			
-			$root.animate({scrollTop: targetOffset}, "fast");
-		}
-	}
-	
-	function _setupTableOfContentsPosition() {
-		$window.scroll(function() {
-			if ($document.scrollTop() > 88) {
-				$body.addClass("pin-table-of-contents");
-			} else {
-				$body.removeClass("pin-table-of-contents");
-			}
-		});
-	}
-
-	function _loadWithNoSecurityCheck(address) {
-		var pagePath = pageList.getPagePath(address);
-	
-		if (currentPage != pagePath) {
-			$pageMain.load("pages/" + pagePath + ".html #page-main-content", function() {
-				_updateMenuStatus(pagePath);
-				SyntaxHighlighter.highlight();
-				_setupTableOfContentsPosition();
+			if (anchor != null) {
+				var targetOffset = $("#" + anchor).offset().top - 20;
 				
-				_scrollToAnchor(address);
-				currentPage = pagePath;
-			});
-		} else {
-			_scrollToAnchor(address);
+				$root.animate({scrollTop: targetOffset}, "fast");
+			}
 		}
-	}
-	
-	function _updateMenuStatus(address) {
-		$pageMenu.find("a.selected").each(function() {
-			$(this).removeClass("selected");
-		});
 		
-		$pageMenu.find("a[href=#" + pageList.getPagePath(address) + "]").addClass("selected");
-	}
-
-	function load(address) {
-		if (pageList.isPageEnabled(address)) {
-			_loadWithNoSecurityCheck(address);
-		} else {
-			_loadWithNoSecurityCheck("not-found");
+		function _setupTableOfContentsPosition() {
+			$window.scroll(function() {
+				if ($document.scrollTop() > 88) {
+					$body.addClass("pin-table-of-contents");
+					
+					if (scrollToTopVisible == false) {
+						scrollToTopVisible = true;
+						$scrollToTop.fadeIn("fast");
+					}
+				} else {
+					$body.removeClass("pin-table-of-contents");
+					
+					if (scrollToTopVisible) {
+						scrollToTopVisible = false;
+						$scrollToTop.fadeOut("fast");
+					}
+				}
+			});
 		}
-	}
 
-	this.get('#:name', function() {
-			load(this.params['name']);
-		});
+		function _loadWithNoSecurityCheck(address) {
+			var pagePath = pageList.getPagePath(address);
+		
+			if (currentPage != pagePath) {
+				$pageMain.load("pages/" + pagePath + ".html #page-main-content", function() {
+					_updateMenuStatus(pagePath);
+					SyntaxHighlighter.highlight();
+					_setupTableOfContentsPosition();
+					
+					_scrollToAnchor(address);
+					currentPage = pagePath;
+				});
+			} else {
+				_scrollToAnchor(address);
+			}
+		}
+		
+		function _updateMenuStatus(address) {
+			$pageMenu.find("a.selected").each(function() {
+				$(this).removeClass("selected");
+			});
+			
+			$pageMenu.find("a[href=#" + pageList.getPagePath(address) + "]").addClass("selected");
+		}
+
+		function load(address) {
+			if (pageList.isPageEnabled(address)) {
+				_loadWithNoSecurityCheck(address);
+			} else {
+				_loadWithNoSecurityCheck("not-found");
+			}
+		}
+	
+		function setupScrollTopLink() {
+			var rootCp = $root;
+		
+			$scrollToTop.click(function(event) {
+				event.preventDefault();
+				
+				$root.animate({scrollTop: 0}, "fast");
+			});
+		}
+		
+		setupScrollTopLink();
+
+		this.get('#:name', function() {
+				load(this.params['name']);
+			});
 	});
 
 	function loadModuleCssFile(pageName) {
