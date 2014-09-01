7b6b0aa287f187b2c499cd73565f4b3c5f4185a0
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-09-01 23:14:54 +0200

    Outdated browser page added

--- js/main.js
+++ js/main.js
@@ -1,116 +1,135 @@
-$(document).ready(function() {
-	var app = Sammy('#page-main', function() {
-		var $pageMain = $("#page-main");
-		var $pageMenu = $("#page-menu");
-		var $root = $("html,body");
-		var $body = $("body");
-		var $window = $(window);
-		var $document = $(document);
-		var $scrollToTop = $("#scroll-to-top");
-		var scrollToTopVisible = false;
+function getInternetExplorerVersion() {
+	var undef,
+		v = 3,
+		div = document.createElement('div'),
+		all = div.getElementsByTagName('i');
+
+	while (
+		div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+		all[0]
+	);
+
+	return v > 4 ? v : undef;
+
+};
 	
-		var currentPage = null;
+if (getInternetExplorerVersion() < 9) {
+	window.location.href = "outdated-browser.html";
+} else {
+	$(document).ready(function() {
+		var app = Sammy('#page-main', function() {
+			var $pageMain = $("#page-main");
+			var $pageMenu = $("#page-menu");
+			var $root = $("html,body");
+			var $body = $("body");
+			var $window = $(window);
+			var $document = $(document);
+			var $scrollToTop = $("#scroll-to-top");
+			var scrollToTopVisible = false;
 		
-		function _scrollToAnchor(address) {
-			var anchor = pageList.getAnchorOnPage(address);
+			var currentPage = null;
 			
-			if (anchor != null) {
-				var targetOffset = $("#" + anchor).offset().top - 20;
+			function _scrollToAnchor(address) {
+				var anchor = pageList.getAnchorOnPage(address);
 				
-				$root.animate({scrollTop: targetOffset}, "fast");
-			}
-		}
-		
-		function _setupTableOfContentsPosition() {
-			$window.scroll(function() {
-				if ($document.scrollTop() > 88) {
-					$body.addClass("pin-table-of-contents");
+				if (anchor != null) {
+					var targetOffset = $("#" + anchor).offset().top - 20;
 					
-					if (scrollToTopVisible == false) {
-						scrollToTopVisible = true;
-						$scrollToTop.fadeIn("fast");
-					}
-				} else {
-					$body.removeClass("pin-table-of-contents");
-					
-					if (scrollToTopVisible) {
-						scrollToTopVisible = false;
-						$scrollToTop.fadeOut("fast");
-					}
+					$root.animate({scrollTop: targetOffset}, "fast");
 				}
-			});
-		}
-
-		function _loadWithNoSecurityCheck(address) {
-			var pagePath = pageList.getPagePath(address);
-		
-			if (currentPage != pagePath) {
-				$pageMain.load("pages/" + pagePath + ".html #page-main-content", function() {
-					_updateMenuStatus(pagePath);
-					SyntaxHighlighter.highlight();
-					_setupTableOfContentsPosition();
-					
-					_scrollToAnchor(address);
-					currentPage = pagePath;
-				});
-			} else {
-				_scrollToAnchor(address);
 			}
-		}
-		
-		function _updateMenuStatus(address) {
-			$pageMenu.find("a.selected").each(function() {
-				$(this).removeClass("selected");
-			});
 			
-			$pageMenu.find("a[href=#" + pageList.getPagePath(address) + "]").addClass("selected");
+			function _setupTableOfContentsPosition() {
+				$window.scroll(function() {
+					if ($document.scrollTop() > 88) {
+						$body.addClass("pin-table-of-contents");
+						
+						if (scrollToTopVisible == false) {
+							scrollToTopVisible = true;
+							$scrollToTop.fadeIn("fast");
+						}
+					} else {
+						$body.removeClass("pin-table-of-contents");
+						
+						if (scrollToTopVisible) {
+							scrollToTopVisible = false;
+							$scrollToTop.fadeOut("fast");
+						}
+					}
+				});
+			}
+
+			function _loadWithNoSecurityCheck(address) {
+				var pagePath = pageList.getPagePath(address);
+			
+				if (currentPage != pagePath) {
+					$pageMain.load("pages/" + pagePath + ".html #page-main-content", function() {
+						_updateMenuStatus(pagePath);
+						SyntaxHighlighter.highlight();
+						_setupTableOfContentsPosition();
+						
+						_scrollToAnchor(address);
+						currentPage = pagePath;
+					});
+				} else {
+					_scrollToAnchor(address);
+				}
+			}
+			
+			function _updateMenuStatus(address) {
+				$pageMenu.find("a.selected").each(function() {
+					$(this).removeClass("selected");
+				});
+				
+				$pageMenu.find("a[href=#" + pageList.getPagePath(address) + "]").addClass("selected");
+			}
+
+			function load(address) {
+				if (pageList.isPageEnabled(address)) {
+					_loadWithNoSecurityCheck(address);
+				} else {
+					_loadWithNoSecurityCheck("not-found");
+				}
+			}
+			
+			function setupScrollTopLink() {
+				var rootCp = $root;
+			
+				$scrollToTop.click(function(event) {
+					event.preventDefault();
+					
+					$root.animate({scrollTop: 0}, "fast");
+				});
+			}
+			
+			setupScrollTopLink();
+
+			this.get('#:name', function() {
+					load(this.params['name']);
+				});
+		});
+
+		function loadModuleCssFile(pageName) {
+			var cssLocation = "css/" + pageName + ".css";
+		
+			$.get(cssLocation, function(css) {
+			   $('<style type="text/css"></style>')
+				  .html(css)
+				  .appendTo("head");
+			});
 		}
 
-		function load(address) {
-			if (pageList.isPageEnabled(address)) {
-				_loadWithNoSecurityCheck(address);
-			} else {
-				_loadWithNoSecurityCheck("not-found");
+		function loadAllModuleCssFiles() {
+			var pages = pageList.getPageList();
+			
+			for (i = 0; i < pages.length; i++) {
+				loadModuleCssFile(pages[i]);
 			}
 		}
-	
-		function setupScrollTopLink() {
-			var rootCp = $root;
 		
-			$scrollToTop.click(function(event) {
-				event.preventDefault();
-				
-				$root.animate({scrollTop: 0}, "fast");
-			});
-		}
+		loadAllModuleCssFiles();
 		
-		setupScrollTopLink();
-
-		this.get('#:name', function() {
-				load(this.params['name']);
-			});
+		// start the application
+		app.run('#home');
 	});
-
-	function loadModuleCssFile(pageName) {
-		var cssLocation = "css/" + pageName + ".css";
-	
-		$.get(cssLocation, function(css) {
-		   $('<style type="text/css"></style>')
-			  .html(css)
-			  .appendTo("head");
-		});
-	}
-
-	function loadAllModuleCssFiles() {
-		var pages = pageList.getPageList();
-		
-		for (i = 0; i < pages.length; i++) {
-			loadModuleCssFile(pages[i]);
-		}
-	}
-	
-	loadAllModuleCssFiles();
-	
-	// start the application
-	app.run('#home');
-});
+}
