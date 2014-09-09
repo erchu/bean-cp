fdedcf11ad95dc10004a319348b681df4af2e6c0
Rafal Chojnacki <rafal.chojnacki@gmail.com>
2014-09-09 21:32:05 +0200

    Table of contents - marking selected item

--- js/navigation.js
+++ js/navigation.js
@@ -18,6 +18,8 @@ if (getInternetExplorerVersion() < 8) {
 } else {
 	$(document).ready(function() {
 		var app = Sammy('#page-main', function() {
+			var SCROLL_TOP_OFFSET = 20;
+		
 			var $pageMain = $("#page-main");
 			var $pageMenu = $("#page-menu");
 			var $root = $("html,body");
@@ -26,6 +28,9 @@ if (getInternetExplorerVersion() < 8) {
 			var $document = $(document);
 			var $scrollToTop = $("#scroll-to-top");
 			var scrollToTopVisible = false;
+			
+			var tableOfContentItems;
+			var currentTableOfContentsSelectedItem = null;
 		
 			var currentPage = null;
 			
@@ -33,15 +38,55 @@ if (getInternetExplorerVersion() < 8) {
 				var anchor = pageList.getAnchorOnPage(address);
 				
 				if (anchor != null) {
-					var targetOffset = $("#" + anchor).offset().top - 20;
+					var targetOffset = $("#" + anchor).offset().top - SCROLL_TOP_OFFSET;
 					
 					$root.animate({scrollTop: targetOffset}, "fast");
 				}
 			}
 			
-			function _setupTableOfContentsPosition() {
+			function _pageIsScrolledToBottom() {
+				return $(window).scrollTop() + $(window).height() == $(document).height();
+			}
+			
+			function _markTableOfContentsSelectedItem(scrollPosition) {
+				if (!tableOfContentItems) {
+					return;
+				}
+				
+				var newCurrentTableOfContentItem = null;
+				
+				if (_pageIsScrolledToBottom()) {
+					// mark last element even if it not covers full page
+					newCurrentTableOfContentItem = tableOfContentItems[tableOfContentItems.length - 1];
+				} else {
+					for (i = 0; i < tableOfContentItems.length; i++) {
+						var iValue = tableOfContentItems[i];
+						
+						if (iValue.topFrom <= scrollPosition && scrollPosition <= iValue.topTo) {
+							newCurrentTableOfContentItem = iValue;
+							break;
+						}
+					}
+				}
+				
+				if (newCurrentTableOfContentItem) {
+					if (!currentTableOfContentsSelectedItem || newCurrentTableOfContentItem.id !== currentTableOfContentsSelectedItem.id) {
+						$("section.table-of-contents li.selected").removeClass("selected");
+						$("section.table-of-contents li[data-header-id=\"" + newCurrentTableOfContentItem.id + "\"]").addClass("selected");
+						
+						$("section.table-of-contents li.selected-first-level").removeClass("selected-first-level");
+						$("section.table-of-contents li[data-header-id=\"" + newCurrentTableOfContentItem.selectedFirstLevelId + "\"]").addClass("selected-first-level");
+					
+						currentTableOfContentsSelectedItem = newCurrentTableOfContentItem;
+					}
+				}
+			}
+			
+			function _setupDocumentScrollHandler() {
 				$window.scroll(function() {
-					if ($document.scrollTop() > 88) {
+					var documentScrollTop = $document.scrollTop();
+					
+					if (documentScrollTop > 88) {
 						$body.addClass("pin-table-of-contents");
 						
 						if (scrollToTopVisible == false) {
@@ -56,7 +101,99 @@ if (getInternetExplorerVersion() < 8) {
 							$scrollToTop.fadeOut("fast");
 						}
 					}
+					
+					_markTableOfContentsSelectedItem(documentScrollTop);
 				});
+			}
+			
+			function _generateTableOfContentsItems() {
+				var $tableOfContents = $("section.table-of-contents");
+				tableOfContentItems = [];
+				
+				if ($tableOfContents.length > 0) {
+					var previousItem = null;
+					var previousItemOffsetTop = null;
+					var documentHeight = $(document).height();
+						
+					$("h2,h3").each(function() {
+						var $i = $(this);
+						var iOffsetTop = $i.offset().top - SCROLL_TOP_OFFSET;
+						
+						var newItem = {
+							id: $i.attr("id"),
+							name: $i.text(),
+							type: $i.prop('tagName'),
+							topFrom: previousItemOffsetTop || 0,
+							topTo: documentHeight	// temporary assuming that there is no next item
+						};
+						
+						tableOfContentItems.push(newItem);
+						
+						if (previousItem) {
+							previousItem.topTo = iOffsetTop - 1;	// previous item is not last item
+						}
+						
+						previousItemOffsetTop = iOffsetTop;
+						previousItem = newItem;
+					});
+					
+					if (tableOfContentItems.length > 0) {
+						var html = [];
+						var lastItemType = "";
+						var selectedFirstLevelId = null;
+						
+						html.push("<div class=\"table-of-contents\">\n");
+						html.push("<ul>\n");
+						
+						for (i = 0; i < tableOfContentItems.length; i++) {
+							var iValue = tableOfContentItems[i];
+							
+							if (lastItemType === iValue.type) {
+								html.push("</li>\n");	// close previous item on the same level
+							}
+							
+							if (lastItemType === "H2" && iValue.type === "H3") {
+								html.push("\n<ul>\n");	// start second level
+							}
+							
+							if (lastItemType === "H3" && iValue.type === "H2") {
+								html.push("</ul>\n</li>\n");	// close second level
+							}
+							
+							if (iValue.type === "H2") {
+								selectedFirstLevelId = iValue.id;
+							}
+							
+							iValue.selectedFirstLevelId = selectedFirstLevelId;
+							
+							html.push("<li data-header-id=\"" + iValue.id + "\"><a href=\"#" + currentPage + "," + iValue.id + "\">" + iValue.name + "</a>");
+							
+							lastItemType = iValue.type;
+						}
+						
+						if (lastItemType === "H3") {
+							// close second level
+							html.push("</ul>\n");
+							html.push("</li>\n");
+						}
+							
+						html.push("</li>\n");	// close last item (on first level)
+						
+						html.push("</ul>\n");
+						html.push("</div>\n");
+						
+						$tableOfContents.html(html.join(""));
+					} else {
+						$tableOfContents.css("display", "none");
+					}
+				}
+			}
+			
+			function _setupTableOfContents() {
+				_generateTableOfContentsItems();
+				
+				currentTableOfContentsSelectedItem = null;
+				_markTableOfContentsSelectedItem(window.pageYOffset);
 			}
 
 			function _loadWithNoSecurityCheck(address) {
@@ -66,10 +203,11 @@ if (getInternetExplorerVersion() < 8) {
 					$pageMain.load("pages/" + pagePath + ".html #page-main-content", function() {
 						_updateMenuStatus(pagePath);
 						SyntaxHighlighter.highlight();
-						_setupTableOfContentsPosition();
 						
 						_scrollToAnchor(address);
 						currentPage = pagePath;
+						
+						_setupTableOfContents();
 					});
 				} else {
 					_scrollToAnchor(address);
@@ -83,16 +221,8 @@ if (getInternetExplorerVersion() < 8) {
 				
 				$pageMenu.find("a[href=#" + pageList.getPagePath(address) + "]").addClass("selected");
 			}
-
-			function load(address) {
-				if (pageList.isPageEnabled(address)) {
-					_loadWithNoSecurityCheck(address);
-				} else {
-					_loadWithNoSecurityCheck("not-found");
-				}
-			}
 			
-			function setupScrollTopLink() {
+			function _setupScrollTopLink() {
 				var rootCp = $root;
 			
 				$scrollToTop.click(function(event) {
@@ -101,12 +231,21 @@ if (getInternetExplorerVersion() < 8) {
 					$root.animate({scrollTop: 0}, "fast");
 				});
 			}
+
+			function _loadPage(address) {
+				if (pageList.isPageEnabled(address)) {
+					_loadWithNoSecurityCheck(address);
+				} else {
+					_loadWithNoSecurityCheck("not-found");
+				}
+			}
 			
-			setupScrollTopLink();
+			_setupScrollTopLink();
+			_setupDocumentScrollHandler();
 
 			this.get('#:name', function() {
-					load(this.params['name']);
-				});
+				_loadPage(this.params['name']);
+			});
 		});
 		
 		// start the application
